e211c963ff12894380032ebf9b3693a5d42e2ef7
Alison Manning <alisonlaurenmanning@gmail.com>
2020-12-19 07:45:08 -0600

    add

--- HR_forinterviews1.js
+++ HR_forinterviews1.js
@@ -1,5 +1,19 @@
-const numbers = [10, 20, 30, 40]
-const add = (a, b) =>
-a + b
-const sum = numbers.reduce(add)
-console.log(sum)
+//set the depth to zero, since its starting at the root
+function nodeDepths(root, depth = 0) {
+    if (root === null) return 0;
+    //depth + the distance of the node to the left, adding one each time
+    //same for the right, gets grand sum stored in "depth"
+    return depth + nodeDepths(root.left, depth + 1) + nodeDepths(root.right, depth + 1)
+}
+
+// This is the class of the input binary tree.
+class BinaryTree {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+// Do not edit the line below.
+exports.nodeDepths = nodeDepths;
