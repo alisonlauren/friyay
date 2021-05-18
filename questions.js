c8ed77d16c61c4330efcc55a3454238b084edbdd
Alison Manning <alisonlaurenmanning@gmail.com>
2021-05-18 11:09:41 -0500

    more

---
+++ questions.js
@@ -0,0 +1,28 @@
+//1. Live Code Solution 1:
+//Given an array of random srings, Write a function that will return a new array 
+//with only the words that contain the letter e.
+
+randomStrings = ['elephant', 'cat', 'pEnguin', 'bird', 'dog', 'rat', 'lion', 'parrot']
+
+
+
+
+//2. Live Code Solution 2:
+// Given an array of random strings. Write a function that will return a new array
+//with only the words that are 4 letters or more.
+
+
+
+
+// Independent Code Solution 1:
+
+// Write a function that takes two agruements, String1 and String2. Join these strings together
+// and reverse the newString.
+
+
+
+
+
+//Independent Code Solution 2:
+//Write a function that, given a string, will always return the 4th charachter. And if
+//the string is too short, the console will print "too short of a string".
